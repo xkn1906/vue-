a87105f1481a5e53db45a5ba250f6581be373d8c
徐珂妮 <591613398@qq.com>
2021-01-12 11:09:16 +0800

    111

--- hello-world/app/src/main.js
+++ hello-world/app/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+const routes = [{
+    path: '/test',
+    component: () =>
+        import ('./page/testVue.vue')
+}]
+const router = new VueRouter({
+    routes
+})
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ hello-world/app/src/page/testVue.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class=''>
+        hello
+    </div>
+</template>
+
+<script>
+    //import x from ''
+    export default {
+        components: {
+
+        },
+        data() {
+            return {
+
+            };
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        },
+        created() {
+
+        },
+    }
+</script>
+
+<style lang='scss' src=''>
+
+</style>
